af7b2d16852b2042dcc68055b00ebf85363b656c
Vedansh <vedansh.shetti@gmail.com>
2026-01-09 22:55:24 +0100

    1.1.0 Update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -19,4 +19,11 @@ export type PromiseType<T = any> = Promise<T>;
 export type UUIDV4 = `${String}-${String}-4${String}-${"8" | "9" | "A" | "B"}${String}-${String}`;
 type TrustedEmailProviders = "gmail.com" | "yahoo.com" | "yahoo.in" | "outlook.com" | "live.com" | "hotmail.com" | "protonmail.com" | "icloud.com" | "aol.com";
 export type TrustableEmail = `${String}@${TrustedEmailProviders}`;
+export type Exact<T, U extends T = T> = T & {
+    [K in Exclude<keyof U, keyof T>]: never;
+};
+declare const brand: unique symbol;
+export type Brand<T, Name> = T & {
+    [brand]: Name;
+};
 export {};
